038fd4aae09227e232ec5acda08e0743f3a45e59
Shubbair <hussainsalih.github@gmail.com>
2021-08-01 17:13:55 +0300

    commit to main

--- lib/logins.dart
+++ lib/logins.dart
@@ -3,36 +3,64 @@ import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({Key? key}) : super(key: key);
+    bool _isHidden = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: 
-      Column(
-        children:[
-          Text('Register',style: TextStyle(color: Colors.red),),
-          Divider(height: 100,),
-      Container(
-      child:  TextFormField(
-            decoration: InputDecoration(
-                prefixIcon: Icon(
-                  Icons.person,
-                  color: Colors.white,
-                ),
-                border: OutlineInputBorder(
-                  // width: 0.0 produces a thin "hairline" border
-                  borderRadius: BorderRadius.all(Radius.circular(90.0)),
-                  borderSide: BorderSide.none,
-                  //borderSide: const BorderSide(),
-                ),
-
-                hintStyle: TextStyle(color: Colors.white,fontFamily: "WorkSansLight"),
-                filled: true,
-                fillColor: Colors.grey,
-                hintText: 'Password'),
+      body: Column(
+        children: [
+          Divider(
+            height: 100,
           ),
-    ),
-      ],
+          Container(
+            width: 350,
+            child: TextFormField(
+              decoration: InputDecoration(
+                  // prefixIcon: Icon(
+                  //   Icons.person,
+                  //   color: Colors.white,
+                  // ),
+                  border: OutlineInputBorder(
+                    // width: 0.0 produces a thin "hairline" border
+                    borderRadius: BorderRadius.all(Radius.circular(90.0)),
+                    borderSide: BorderSide.none,
+                    //borderSide: const BorderSide(),
+                  ),
+                  hintStyle: TextStyle(
+                      color: Colors.white, fontFamily: "WorkSansLight"),
+                  filled: true,
+                  fillColor: Colors.grey,
+                  hintText: 'Email'),
+            ),
+          ),
+          Container(
+            width: 350,
+            child: TextFormField(
+              obscureText: _isHidden,
+              decoration: InputDecoration(
+                  suffixIcon: Icon(
+                    Icons.visibility,
+                    color: Colors.white,
+                  ),
+                  // prefixIcon: Icon(
+                  //   Icons.person,
+                  //   color: Colors.white,
+                  // ),
+                  border: OutlineInputBorder(
+                    // width: 0.0 produces a thin "hairline" border
+                    borderRadius: BorderRadius.all(Radius.circular(90.0)),
+                    borderSide: BorderSide.none,
+                    //borderSide: const BorderSide(),
+                  ),
+                  hintStyle: TextStyle(
+                      color: Colors.white, fontFamily: "WorkSansLight"),
+                  filled: true,
+                  fillColor: Colors.grey,
+                  hintText: 'Password'),
+            ),
+          ),
+        ],
       ),
     );
   }
