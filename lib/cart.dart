f8ba15b03944b970084a962e3fa162fab916690a
Shubbair <hussainsalih.github@gmail.com>
2021-08-12 15:22:27 +0300

    editing cart widget

--- lib/cart.dart
+++ lib/cart.dart
@@ -15,27 +15,30 @@ class CartScreen extends StatelessWidget {
         elevation: 0.0,
       ),
       body: 
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              margin: EdgeInsets.only(top: 30),
-              width: 350,
-              height: 70,
-              child: 
-              Row(
-                children: [
-                  Container(width: 80,height: 80, child: Image.asset('images/bannana.png'),margin: EdgeInsets.only(left:20),),
-                  Container(child: Text('images/bannana.png'),margin: EdgeInsets.only(left:10),),
-                  Container(child: Icon(Icons.payment),margin: EdgeInsets.only(left:20),),
-                  Container(child: Icon(Icons.remove_circle),margin: EdgeInsets.only(left:20),)
-                ],
-              ),
-             decoration: BoxDecoration(
-          color: Colors.grey[200],
-          borderRadius: BorderRadius.all(Radius.circular(8))),
+        Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                margin: EdgeInsets.only(top: 30),
+                width: 350,
+                height: 70,
+                child: 
+                Row(
+                  children: [
+                    Container(width: 80,height: 80, child: Image.asset('images/bannana.png'),margin: EdgeInsets.only(left:20),),
+                    Container(child: Text('images/bannana.png'),margin: EdgeInsets.only(left:10),),
+                    Container(child:IconButton(onPressed: (){}, icon: Icon(Icons.payment)),margin: EdgeInsets.only(left:5),),
+                    Container(child: IconButton(onPressed: (){}, icon: Icon(Icons.remove_circle)),margin: EdgeInsets.only(left:5),)
+                  ],
+                ),
+               decoration: BoxDecoration(
+            color: Colors.grey[200],
+            borderRadius: BorderRadius.all(Radius.circular(8))
             ),
-          ],
+              ),
+            ],
+          ),
         ),
     );
   }
