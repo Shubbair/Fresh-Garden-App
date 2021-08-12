0aede5a325cef71edbe25d14cddc5372a7eaa74e
Shubbair <hussainsalih.github@gmail.com>
2021-08-12 14:50:21 +0300

    adding some edite

--- lib/categiory.dart
+++ lib/categiory.dart
@@ -9,52 +9,34 @@ class DataScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: true,
-      drawer: Drawer(
-        // Add a ListView to the drawer. This ensures the user can scroll
-        // through the options in the drawer if there isn't enough vertical
-        // space to fit everything.
-        child: ListView(
-          // Important: Remove any padding from the ListView.
-          padding: EdgeInsets.zero,
-          children: [
-            const DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.blue,
-              ),
-              child: Text('Drawer Header'),
-            ),
-            ListTile(
-              title: const Text('Item 1'),
-              onTap: () {
-                // Update the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              title: const Text('Item 2'),
-              onTap: () {
-                // Update the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
+     drawer: Drawer(  
+        child: ListView(  
+          padding: EdgeInsets.zero,  
+          children: <Widget>[  
+            UserAccountsDrawerHeader(  
+                decoration: BoxDecoration(
+    color: Colors.green[300],
+  ),
+              accountName: Text("Joe Harvard"),  
+              accountEmail: Text("example@example.com"),  
+              currentAccountPicture: CircleAvatar(  
+                backgroundColor: Colors.orange,  
+                child: Icon(Icons.person),
+              ),  
+          
+            ),  
+            ListTile(  
+              leading: Icon(Icons.logout), title: Text("Logout"),  
+              onTap: () {  
+                  Navigator.pushNamed(context, '/login');
+              },  
+            ),  
           ],
         ),
-      ),
+     ),
       appBar: AppBar(
+        iconTheme: IconThemeData(color: Colors.black87),
         elevation: 0.0,
-        leading: IconButton(
-            onPressed: () {
-              // Scaffold.of(context).openDrawer();
-             },
-            icon: Icon(
-              Icons.list,
-              color: Colors.black87,
-              size: 28.0,
-            )),
         backgroundColor: Color(0XFFFAFAFA),
         actions: [
           Padding(
@@ -94,7 +76,6 @@ class DataScreen extends StatelessWidget {
         ],
         // IconButton(icon: Icon(Icons.shopping_cart), onPressed: () {},color: Colors.black,),
       ),
-      //SizedBox(child: Image(image: new AssetImage('images/home.png'),),width:280,)
       body: Column(
         children: [
           Row(
